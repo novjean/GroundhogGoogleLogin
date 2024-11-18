80d9f70ce2052e4a3eedc837193991c66296cdbf
Novjean J Kannathara <novjeanj@gmail.com>
2024-11-18 21:54:48 +0530

    google log in logic

--- app/src/main/java/com/novatech/groundhoggooglelogin/MainActivity.kt
+++ app/src/main/java/com/novatech/groundhoggooglelogin/MainActivity.kt
@@ -20,28 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             GroundhogGoogleLoginTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GroundhogGoogleLoginTheme {
-        Greeting("Android")
-    }
-}
+
