18cb3d3d61e55c0671782638c509b3253ea29e8e
Novjean J Kannathara <novjeanj@gmail.com>
2024-11-18 20:23:59 +0530

    dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,13 +81,14 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.6.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
 
+    // coil-compose to load images remotely
     implementation("io.coil-kt:coil-compose:2.2.2")
 
     // Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.52")
     kapt("com.google.dagger:hilt-android-compiler:2.52")
-    implementation("androidx.hilt:hilt-work:1.0.0")
+    //implementation("androidx.hilt:hilt-work:1.0.0")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
 }
